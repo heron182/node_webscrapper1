c935457b4063fe2a4aad19c6eb8abc6cd0ab0aaf
Heron <heron.rossi@hotmail.com>
2017-02-18 15:00:39 +0000

    created scrapper with static url

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app = express();
 var url = 'http://www.comparegames.com.br/comprar/ps4';
 
 request(url, function(error, response, html) {
 
-            if (!error) {
-                var $ = cheerio.load(html);
+    if (!error) {
+        var $ = cheerio.load(html);
+        var arr = [];
+        $('li.gm-ctn').each(function(i, ele) {
+            title = $(this).find('p.name').text();
+            metacritic = !!$(this).find('p.mtrc').text() ? $(this).find('p.mtrc').text().replace(/\D/g,'') : 'N/A';
+            currency = $(this).find('span.cg-prc').text().replace(/\s/g, '');
+            game_obj = {
+                title: title,
+                metacritic: metacritic,
+                currency: currency
+            };
+            arr.push(game_obj);
+        });
 
-                $('.header').filter(function() {
+        fs.writeFile('output.json', JSON.stringify(arr, null, 4), function(err) {
 
-                });
+            console.log('Json file written succesfully!');
 
+        });
+    }
 
-                fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-
-                    console.log('File successfully written! - Check your project directory for the output.json file');
-
-                });
-            }
+});
